5a6d6a2828220e7058568bc1ff79a53149674372
Iris Huang <lyaui@outloook.com>
2021-11-09 19:14:57 +0800

    新增 footer 樣式

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,5 +1,24 @@
 const Footer = () => {
-  return <div>FOOTER</div>;
+  const infos = [
+    {
+      name: 'Breakfast',
+      work: 'designer',
+      link: 'https://2021.thef2e.com/users/6296427084285739247',
+    },
+    { name: 'Iris', work: 'developer', link: 'https://2021.thef2e.com/users/6296427084285739074' },
+  ];
+  return (
+    <footer className='h-14 bg-primary flex items-center justify-center'>
+      <div className='text-center text-white'>
+        #The F2E 3rd Week01
+        {infos.map((person) => (
+          <a className='px-2 underline' href={person.link}>
+            #{person.work}-{person.name}
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

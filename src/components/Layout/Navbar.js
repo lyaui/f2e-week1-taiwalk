3d6ded77ebf51692b25fb3149809011ef95d1beb
Iris Huang <lyaui@outloook.com>
2021-11-09 21:43:01 +0800

    新增 category constant

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,14 +1,9 @@
 import { NavLink, Link } from 'react-router-dom';
 import { ROUTE } from 'constants/routes.js';
+import { CATEGORIES } from 'constants/categories.js';
 import DesktopLogo from 'assets/Logo-desktop.svg';
 
 const Navbar = () => {
-  const navItems = [
-    { name: '探索景點', type: 'spots' },
-    { name: '節慶活動', type: 'activities' },
-    { name: '品嚐美食', type: 'foods' },
-  ];
-
   return (
     <nav className='border-b border-gray-300'>
       <div className='max-w-screen-xl mx-auto flex justify-between items-center h-20 px-12'>
@@ -17,9 +12,9 @@ const Navbar = () => {
         </Link>
 
         <div>
-          {navItems.map((navItem) => (
+          {CATEGORIES.map((navItem) => (
             <NavLink
-              to={ROUTE.BUILD_EXPLORE_PATH({ type: navItem.type })}
+              to={ROUTE.BUILD_EXPLORE_PATH({ type: navItem.value })}
               className='p-2.5 text-gray-700 font-normal'
               // className={['px-2.5', (navData) => navData.isActive && 'active-class']}
             >
---
+++ src/constants/categories.js
@@ -0,0 +1,5 @@
+export const CATEGORIES = [
+  { label: '探索景點', value: 'spots' },
+  { label: '節慶活動', value: 'activities' },
+  { label: '品嚐美食', value: 'foods' },
+];

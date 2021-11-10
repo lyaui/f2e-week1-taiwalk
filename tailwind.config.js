fedd476936e4d0d314c4e4c9303d6e95e2ebcd8f
Iris Huang <lyaui@outloook.com>
2021-11-10 21:04:28 +0800

    新增題選擇背景圖

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,30 @@ module.exports = {
       fontFamily: {
         playfair: ['Playfair Display', 'system-ui'],
       },
+      backgroundImage: {
+        // spots
+        'topic-naturalSpot': "url('assets/images/topics/naturalSpot.png')",
+        'topic-tourismFactory': "url('assets/images/topics/tourismFactory.png')",
+        'topic-rest': "url('assets/images/topics/rest.png')",
+        'topic-leisureAgriculture': "url('assets/images/topics/leisureAgriculture.png')",
+        'topic-ecological': "url('assets/images/topics/ecological.png')",
+        'topic-hotSpring': "url('assets/images/topics/hotSpring.png')",
+        'topic-historicalSite': "url('assets/images/topics/historicalSite.png')",
+        // activities
+        'topic-festival': "url('assets/images/topics/festival.png')",
+        'topic-bike': "url('assets/images/topics/bike.png')",
+        'topic-leisure': "url('assets/images/topics/leisure.png')",
+        'topic-industry': "url('assets/images/topics/industry.png')",
+        'topic-annual': "url('assets/images/topics/annual.png')",
+        'topic-seasons': "url('assets/images/topics/seasons.png')",
+        // foods
+        'topic-localCuisine': "url('assets/images/topics/localCuisine.png')",
+        'topic-chineseCuisine': "url('assets/images/topics/chineseCuisine.png')",
+        'topic-dessert': "url('assets/images/topics/dessert.png')",
+        'topic-exoticCuisine': "url('assets/images/topics/exoticCuisine.png')",
+        'topic-souvenir': "url('assets/images/topics/souvenir.png')",
+        'topic-vegetable': "url('assets/images/topics/vegetable.png')",
+      },
     },
   },
   variants: {

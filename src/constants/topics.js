856041767b77d9b6537bb44016cbf070a6aad12c
Iris Huang <lyaui@outloook.com>
2021-11-10 21:13:52 +0800

    新增主題常數

---
+++ src/constants/topics.js
@@ -0,0 +1,39 @@
+export const TOPICS = {
+  spots: {
+    title: '熱門主題',
+    placeHolder: '你想去哪裡？請輸入關鍵字',
+    cates: [
+      { label: '自然風景類', value: 'naturalSpot' },
+      { label: '觀光工廠類', value: 'tourismFactory' },
+      { label: '休憩類', value: 'rest' },
+      { label: '休閒農業類', value: 'leisureAgriculture' },
+      { label: '生態類', value: 'ecological' },
+      { label: '溫泉類', value: 'hotSpring' },
+      { label: '古蹟類', value: 'historicalSite' },
+    ],
+  },
+  activities: {
+    title: '熱門主題',
+    placeHolder: '想找有趣的？請輸入關鍵字',
+    cates: [
+      { label: '節慶活動', value: 'festival' },
+      { label: '自行車活動', value: 'bike' },
+      { label: '遊憩活動', value: 'leisure' },
+      { label: '產業文化活動', value: 'industry' },
+      { label: '年度活動', value: 'annual' },
+      { label: '四季活動', value: 'seasons' },
+    ],
+  },
+  foods: {
+    title: '熱門分類',
+    placeHolder: '你想吃什麼？請輸入關鍵字',
+    cates: [
+      { label: '地方特產', value: 'localCuisine' },
+      { label: '中式美食', value: 'chineseCuisine' },
+      { label: '甜點冰品', value: 'dessert' },
+      { label: '異國料理', value: 'exoticCuisine' },
+      { label: '伴手禮', value: 'souvenir' },
+      { label: '素食', value: 'vegetable' },
+    ],
+  },
+};
